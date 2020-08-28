ead678405f1aba54c81ddd58132059df10cd8c40
user u1 <u1@domain.com>
2020-08-29 00:44:22 +0300

    Add console UI

--- src/SudokuApp.java
+++ src/SudokuApp.java
@@ -10,12 +10,38 @@ public class SudokuApp {
         Field gameField = new Field();
         String exitCode = "exit";
         String line = "";
-        try ( BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-        //try (BufferedReader reader = new BufferedReader(new FileReader(new File("sudoku.txt")))) {
-            //while (!line.equals(exitCode)) {
-
-                //TODO: Написать приветствие
-
+        BufferedReader reader = null;
+        BufferedReader fileReader = null;
+        System.out.println("***Sudoku BruteForce***");
+        try ( BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in))) {
+            while (!line.equals(exitCode))
+            {
+                System.out.println("Для ввода судоку построчно напиши ВВОД\n" +
+                    "Для ввода из файла напиши ФАЙЛ\n" +
+                    "Для выхода из программы напиши ВЫХОД\n");
+                line = consoleReader.readLine();
+                switch (line) {
+                    case "ВВОД": {
+                        reader = consoleReader;
+                        System.out.println("Вводи построчно поле для судоку. На месте пустой ячейки вводи 0");
+                    }
+                    break;
+                    case "ФАЙЛ": {
+                        System.out.println("Введи путь до txt файла с полем судоку\n" +
+                                "Пример: C:/folder/sudoku.txt\n" +
+                                "В файле должно быть поле судоку в виде набора строк с цифрами\n" +
+                                "Каждая строка должна соответствовать строке поля судоку\n" +
+                                "На месте пустой ячейки вводи 0");
+                        line = consoleReader.readLine();
+                        fileReader = new BufferedReader(new FileReader(new File(line)));
+                        reader = fileReader;
+                    }
+                    break;
+                    case "ВЫХОД": {
+                        line = "exit";
+                        continue;
+                    }
+                }
                 for (int row = 0; row < gameField.FIELD_LENGTH; row++) {
                     System.out.println("Введите строчку #" + (row + 1));
                     line = reader.readLine();
@@ -30,11 +56,13 @@ public class SudokuApp {
                 }
                 SudokuBF sbf = new SudokuBF();
                 sbf.build(gameField);
-                //sbf.build(gameField);
-            //}
+            }
         } catch (Exception e) {
             e.printStackTrace();
+            line = "exit";
         }
-
+        System.out.println("Выход из программы Sudoku BruteForce");
     }
+
+
 }
