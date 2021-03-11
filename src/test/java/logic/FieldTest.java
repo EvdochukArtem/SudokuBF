10a8b4c4581c27deb89913189558b3151d4f1550
Artem <evdochukartem@gmail.com>
2021-03-12 00:22:02 +0300

    add tests

---
+++ src/test/java/logic/FieldTest.java
@@ -0,0 +1,86 @@
+package logic;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldTest {
+
+    @Test
+    void readFieldFromFile() {
+        Field testField = new Field();
+        try {
+            testField.readFieldFromFile("sudoku.txt");
+            assertEquals(TestData.SUDOKU_INCOMPLETE, testField.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void setDigitCorrect() {
+        Field testField = new Field();
+        assertTrue(testField.setDigit(1, 0, 0));
+    }
+
+    @Test
+    void setDigitSamePosition() {
+        Field testField = new Field();
+        assertTrue(testField.setDigit(1, 0, 0));
+        assertFalse(testField.setDigit(1, 0, 0));
+    }
+
+    @Test
+    void setDigitToHigh() {
+        Field testField = new Field();
+        assertFalse(testField.setDigit(12, 0, 0));
+    }
+
+    @Test
+    void setDigitToLow() {
+        Field testField = new Field();
+        assertFalse(testField.setDigit(-12, 0, 0));
+    }
+
+    @Test
+    void getCellValue() {
+        Field testField = new Field();
+        try {
+            testField.readFieldFromFile("sudoku.txt");
+            assertEquals(7, testField.getCellValue(1, 0));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testClone() {
+        Field testField = new Field();
+        try {
+            testField.readFieldFromFile("sudoku.txt");
+            Field clonedField = testField.clone();
+            assertEquals(testField, clonedField);
+        } catch (IOException | CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Ignore
+    @Test
+    void testToString() {}
+
+    @Ignore
+    @Test
+    void readFieldFromConsole() {}
+
+    @Ignore
+    @Test
+    void testEquals() {}
+
+    @Ignore
+    @Test
+    void testHashCode() {}
+}
---
+++ src/test/java/logic/TestData.java
@@ -0,0 +1,21 @@
+package logic;
+
+public class TestData {
+
+    //public final static Field SUDOKU_EMPTY = new Field();
+
+    public final static String SUDOKU_INCOMPLETE = "-----------------\n" +
+            "- 7 9|8 - 3|- - -|\n" +
+            "- - -|6 2 -|- 9 -|\n" +
+            "4 - 1|- - 9|8 3 -|\n" +
+            "-----------------\n" +
+            "- 4 -|3 7 -|- 2 -|\n" +
+            "9 - 8|- 6 5|7 4 -|\n" +
+            "1 - -|- 4 -|- 6 -|\n" +
+            "-----------------\n" +
+            "8 - 3|2 - 7|4 1 -|\n" +
+            "7 1 -|- 3 6|2 - -|\n" +
+            "- - -|- - 1|3 - -|\n" +
+            "-----------------\n";
+
+}
